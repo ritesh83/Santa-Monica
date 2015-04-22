6f6d55231feb42c9de185fd0d2319782c06a37c2
Ritesh Keswani <ritesh.keswani@panasonic.aero>
2015-04-21 22:51:37 -0700

    updated router

--- app/router.js
+++ app/router.js
@@ -1,33 +1,17 @@
-import Ember from 'ember';
+
+import CustomRouter from './custom-router';
+
 import config from './config/environment';
 
-var moduleNavigation = Ember.Object.create();
-var route = Ember.RouterDSL.prototype.route;
-
-Ember.RouterDSL.prototype.route = function(name, options) {
-
-    if(options['navLabel'] && options['className']) {
-        var navObj = Ember.Object.create();
-        navObj.setProperties({
-            label: options['navLabel'],
-            route: name,
-            className: options['className']
-        });
-
-        moduleNavigation.set(name, navObj);
-    }
-
-    route.apply(this, arguments);
-};
-
-var Router = Ember.Router.extend({
+var Router = CustomRouter.extend({
     location: config.locationType,
-    moduleNavigation: moduleNavigation
 });
 
 Router.map(function() {
     this.route("venue", {navLabel: "Venue", className: "glyphicon glyphicon-object-align-bottom"});
+
     this.route("talks", {navLabel: "Talks", className: "glyphicon glyphicon-user"});
 });
 
 export default Router;
+
---
+++ copy-script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp dist/index.html /Users/keswanir/work/meetup/chrome/dist/santa-monica
+cp -r dist/assets /Users/keswanir/work/meetup/chrome/dist/santa-monica/assets
